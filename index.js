8415799b2a68b975cb4312eb82c810e411313e26
julaikha27 <jule.x634@gmail.com>
2024-06-18 21:12:13 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+import { accessToken } from "./js/CesiumConfig.js";
+import { locations } from "./js/Location.js";
+import { createSelectElement } from "./js/DropDown.js";
+import { flyToLocation } from "./js/CeiumViewer.js";
+
+// Your access token can be found at: https://ion.cesium.com/tokens.
+// This is the default access token from your ion account
+
+Cesium.Ion.defaultAccessToken = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMDUzOThmMi1kMGI3LTQ3OWMtODMyMS1iNzgwYWUwYzMyNDUiLCJpZCI6MjIyMzUwLCJpYXQiOjE3MTg2ODczMTN9.lfyFCJmraTjgg3Zqbcd9QTGhDQE5jw2EWyEaVoblk6k;
+
+// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+const viewer = new Cesium.Viewer("cesiumContainer", {
+  terrain: Cesium.Terrain.fromWorldTerrain(),
+});
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+const buildingTileset = await Cesium.createOsmBuildingsAsync();
+viewer.scene.primitives.add(buildingTileset);
+
+const options = Object.keys(locations).map((key) => ({
+  value: key,
+  textContent: locations[key].cityName,
+}));
+
+const dropdown = createSelectElement(options, "toolbar");
+
+flyToLocation(viewer, locations[0].coordinate);
+
+if (dropdown) {
+  dropdown.addEventListener("change", (event) => {
+    const selectedIndex = event.target.value;
+    const selectedLocation = Object.values(locations)[selectedIndex].coordinate;
+    flyToLocation(viewer, selectedLocation);
+  });
+}
